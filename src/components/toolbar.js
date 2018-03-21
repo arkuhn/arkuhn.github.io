65cfa2a181e5debb695160f01f39af88dbdb3139
Adam-Kuhn <adam.kuhn@lenel.com>
2018-03-21 15:49:41 -0400

    Replaced contact page with dropdown

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -6,8 +6,7 @@ import {
   } from 'react-router-dom'
 import Projects from './projects_page/projects'
 import Landing from './landing_page/landing'
-import Contact from './contact_page/contact';
-import { Menu } from 'semantic-ui-react'
+import { Menu, Dropdown, Icon } from 'semantic-ui-react'
 
 
 class Toolbar extends Component {
@@ -25,14 +24,30 @@ class Toolbar extends Component {
                     <Menu size='massive' pointing secondary >
                         <Menu.Item as={Link} to="/" name='home' active={activeItem === 'home'} onClick={this.handleItemClick} position={'left'} />
                     
-                        <Menu.Item as={Link} to="projects" name='projects' active={activeItem === 'projects'} onClick={this.handleItemClick} icon='folder open outline' position={'right'} />
+                        <Dropdown item text='Contact' icon ='comments outline' position={'right'}>
+                            <Dropdown.Menu>
+                                    <Dropdown.Item as='a' href='mailto:ark9719@rit.edu'>
+                                        <Icon name='mail outline' size='small'/>
+                                        ark9719@rit.edu
+                                    </Dropdown.Item>
+                                    <Dropdown.Item as='a' href='https://www.linkedin.com/in/adam-r-kuhn/'>
+                                        <Icon  name='linkedin' size='small'/>
+                                        LinkedIn
+                                    </Dropdown.Item>
+                                    <Dropdown.Item as='a' href='https://twitter.com/ark9719'>
+                                        <Icon  name='twitter' size='small'/>
+                                        Twitter
+                                    </Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        
+                        <Menu.Item as={Link} to="projects" name='projects' active={activeItem === 'projects'} onClick={this.handleItemClick} icon='folder open outline' />
                     
-                        <Menu.Item as={Link} to="contact" name='contact' active={activeItem === 'contact'} onClick={this.handleItemClick} icon ='comments outline'/>
+                        
                     </Menu>
 
                     <Route exact path="/" component={Landing}/>
                     <Route path="/projects" component={Projects}/>
-                    <Route path="/contact" component={Contact}/>
                 </div>
             </Router>
         )
